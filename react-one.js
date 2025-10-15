91486ece65658d63214553dfce6e8d77b635c144
TheRemonDev <riyanahmed3536@gmail.com>
2025-10-15 17:02:42 +0100

    if-else

--- react-one.js
+++ react-one.js
@@ -9,3 +9,23 @@ const countrtName = "Bangladesh";
 //countrtName= "India"
 console.log(countrtName);
 //orthat jegula change kora jai na segula k const dara decleard kora hoi
+
+// 02=======If/Else
+
+const num = 20;
+if (num >=20) {
+    console.log("Contraculation");
+    
+} else {
+    console.log("Sorry Try Again");
+    
+}
+
+const nums = 40;
+if (nums >= 20 && nums <=30) {
+    console.log("Wow You Are Winner");
+    //&& mane 2 ta condition e full fil korte hobe || mane jekono ekta
+} else{
+    console.log("Sorry Yaar You Are Looser");
+    
+}
