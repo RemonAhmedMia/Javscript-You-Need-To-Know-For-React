8206d5d594b0f0414b6f2554061112a8d24409b4
TheRemonDev <riyanahmed3536@gmail.com>
2025-10-15 16:21:09 +0100

    variables

---
+++ react-one.js
@@ -0,0 +1,11 @@
+// 01========Variables
+
+let name = "Rimon";
+name = "Rimon Mia";
+console.log(name);
+//orthat je sokol value reasign ba change kora jai segula k let diye decleard kora hoi;
+
+const countrtName = "Bangladesh";
+//countrtName= "India"
+console.log(countrtName);
+//orthat jegula change kora jai na segula k const dara decleard kora hoi
