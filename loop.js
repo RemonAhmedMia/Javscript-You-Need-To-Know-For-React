5f1582c10d55a12e259351492712938ad45ba783
TheRemonDev <riyanahmed3536@gmail.com>
2025-10-16 05:00:42 +0100

    object

--- loop.js
+++ loop.js
@@ -15,3 +15,15 @@ function sum (a,b) {
 
 const final = sum (4,6);
 console.log(final);
+
+// 07: Object==========
+
+const person = {
+    name : 'Rimon',
+    friens : ['Rimo' , 'Simo' , 'Kimo'],
+    age: 10,
+    status: 'Single'
+}
+person.name = "Sadman Riyan Rimon"
+console.log(person.name);
+console.log(person.friens[2]);
