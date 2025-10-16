039859efb4ab7a619e72c40e459d1540c861f001
TheRemonDev <riyanahmed3536@gmail.com>
2025-10-16 04:55:20 +0100

    function and  loop

---
+++ loop.js
@@ -0,0 +1,17 @@
+// **04 For Loop=====
+
+const bondhu = ['Rimon', 'Shakib' , 'Akib'];
+for (let index = 0; index < bondhu.length; index++) {
+    const element = bondhu[index];
+    console.log(element);
+}
+
+// **05=====Function====
+
+function sum (a,b) {
+    const result = a + b;
+    return result;
+}
+
+const final = sum (4,6);
+console.log(final);
